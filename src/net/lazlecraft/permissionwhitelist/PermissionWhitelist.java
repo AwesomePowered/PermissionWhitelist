f4e59ce7ad1eacb864e3ea13dea0d70a9ec71264
LaxWasHere <public@LaxWasHere.com>
2013-09-12 15:51:49 -0700

    Chicken Nuggets

---
+++ src/net/lazlecraft/permissionwhitelist/PermissionWhitelist.java
@@ -0,0 +1,26 @@
+package net.lazlecraft.permissionwhitelist;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerLoginEvent.Result;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PermissionWhitelist extends JavaPlugin implements Listener {
+	
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+		getConfig().options().copyDefaults(true);
+		this.saveConfig();
+	}
+	
+	@EventHandler
+	public void onLogin(PlayerLoginEvent ev) {
+		Player p = ev.getPlayer();
+		if (!p.hasPermission("whitelisted.player")) {
+			ev.disallow(Result.KICK_OTHER, ChatColor.translateAlternateColorCodes('&', getConfig().getString("KickReason").replace("%PLAYER%", p.toString())));
+		}
+	}
+}
